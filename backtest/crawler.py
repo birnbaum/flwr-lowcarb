a68999a7a84b1c1042c6c5a237c16a733330b135
Martin Schellenberger <mail@martin-schellenberger.de>
2022-10-20 11:22:53 +0200

    fixed historic_forecast for the crawler to now query the /emissions/forecasts/batch endpoint

--- backtest/crawler.py
+++ backtest/crawler.py
@@ -1,10 +1,13 @@
 from datetime import timedelta
+from dateutil import parser
 
 import pandas as pd
 
 from carbon_sdk_client.openapi_client.api.carbon_aware_api import CarbonAwareApi
 from carbon_sdk_client.openapi_client.api_client import ApiClient
 from carbon_sdk_client.openapi_client.configuration import Configuration
+from carbon_sdk_client.openapi_client.models import EmissionsForecastDTO, EmissionsForecastBatchParametersDTO
+from carbon_sdk_client.openapi_client import ApiException
 
 
 LOCATIONS = ['westcentralus', 'ukwest', 'uksouth', 'westeurope', 'westus', 'australiacentral', 'australiaeast', 'swedencentral', 'norwaywest', 'norwayeast', 'northeurope', 'centralus', 'francesouth', 'francecentral']
@@ -18,7 +21,7 @@ def backtest_daterange():
     return pd.date_range(start=BACKTEST_START, end=BACKTEST_END, freq=f"{FORECAST_FREQ}min")
 
 
-def historic_forecast(api_instance, location, start_time, end_time) -> pd.DataFrame:
+def historic_forecast_ideal(api_instance, location, start_time, end_time) -> pd.DataFrame:
     api_response = api_instance.get_emissions_data_for_location_by_time(location, time=start_time, to_time=end_time)
     result = pd.DataFrame([r.to_dict() for r in api_response]).drop(columns={"duration"}).sort_values("time")
     result["time"] = result["time"].dt.tz_localize(None)  # remove timezone
@@ -28,6 +31,46 @@ def historic_forecast(api_instance, location, start_time, end_time) -> pd.DataFr
     result = result.set_index(["location", "query_time", "time"])
     return result
 
+def historic_forecast(api_instance, location, start_time, end_time) -> pd.DataFrame:
+    query_time = parser.parse(start_time) if (type(start_time) == str) else start_time
+    start_time = query_time + timedelta(minutes=5)
+    end_time = parser.parse(end_time) if (type(end_time) == str) else end_time
+
+    if type(location) == str:
+        locations = [location]
+    else:
+        locations = location
+
+    emissions_forecast_batch_parameters_dtos = [
+        EmissionsForecastBatchParametersDTO(
+            requested_at=query_time,
+            location=location,
+            data_start_at=start_time,
+            data_end_at=end_time,
+            window_size=5,
+        )
+    for location in locations]
+
+    try:
+        api_response = api_instance.batch_forecast_data_async(emissions_forecast_batch_parameters_dto=emissions_forecast_batch_parameters_dtos, async_req=False)
+
+        results = []
+        for i_entry, entry in enumerate(api_response, 0):
+            result = pd.DataFrame(entry.to_dict()['forecast_data']).drop(columns={"duration"}).sort_values("timestamp")
+            result = result.rename(columns={'timestamp':'time', 'value':'rating'})
+            result["time"] = result["time"].dt.tz_localize(None)  # remove timezone
+            result = result[result["time"] >= start_time]  # api sometimes returns older values than start_time
+            result["location"] = locations[i_entry]  # api returns watttime label instead of input label
+            result["query_time"] = query_time
+            result = result.set_index(["location", "query_time", "time"])
+
+            results.append(result)
+
+
+        return pd.concat(results)
+
+    except ApiException as e:
+        print("Exception when calling CarbonAwareApi->batch_forecast_data_async: %s\n" % e)
 
 def main():
     with ApiClient(configuration=Configuration(host="https://carbon-aware-api.azurewebsites.net")) as api_client:
