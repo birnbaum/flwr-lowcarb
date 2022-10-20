475bed0377268dc070043e66a2e18e462db19304
Philipp Wiesner <wiesner@tu-berlin.de>
2022-10-20 08:47:03 +0200

    implemented carbon-aware strategy

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -1,34 +1,35 @@
-from typing import Dict, Callable, Tuple
-
-import numpy as np
 import pandas as pd
 
+from strategy import Strategy, RandomStrategy, CarbonAwareStrategy
 from crawler import backtest_daterange, LOCATIONS
 
 # TODO: For now we assume one client per location
 
 
-def run_backtest(history_path: str, strategy: Callable[[Dict[str, pd.DataFrame]], str]):
+def run_backtest(history_path: str, strategy: Strategy):
     with open(history_path, "r") as f:
         history = pd.read_csv(f, index_col=[0, 1, 2], parse_dates=True)
 
-    selections = {location: 0 for location in LOCATIONS}  # counts number of times a location was selected
+    participation = {location: 0 for location in LOCATIONS}  # counts number of times a location was selected
     emissions = {location: 0 for location in LOCATIONS}  # counts emissions per location
+    rounds = 0
     for dt in backtest_daterange():
         df = history[history.index.get_level_values(1) == dt]  # level 1 is query time
         df.index = df.index.droplevel(1)
-        location_forecasts = {}
+        location_forecasts = {location: 0 for location in LOCATIONS}  # important for sort order
         for location, location_forecast in df.groupby(level=0):
             location_forecast.index = location_forecast.index.droplevel(0)  # dropping location from index
             location_forecasts[location] = location_forecast
 
-        selected_locations = strategy(location_forecasts)
+        selected_locations = strategy.select(location_forecasts, participation)
         for location in selected_locations:
-            selections[location] += 1
+            participation[location] += 1
             emissions[location] += float(location_forecasts[location].loc[dt])
+        rounds += 1
 
-    return selections, emissions
+    return participation, emissions, rounds
 
 
 if __name__ == "__main__":
-    run_backtest("backtest/history.csv")
+    # run_backtest("backtest/history.csv", strategy=RandomStrategy(3))
+    run_backtest("backtest/history.csv", strategy=CarbonAwareStrategy(clients_per_round=3, max_forecast_duration=36))
---
+++ backtest/strategy.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+import numpy as np
+import pandas as pd
+
+
+class Strategy(ABC):
+    @abstractmethod
+    def select(self, location_forecasts: Dict[str, pd.DataFrame], past_participation: Dict[str, int]) -> List[str]:
+        """Selects a list of locations"""
+
+
+class RandomStrategy(Strategy):
+    def __init__(self, clients_per_round: int):
+        self.clients_per_round = clients_per_round
+
+    def select(self, location_forecasts: Dict[str, pd.DataFrame], past_participation: Dict[str, int]) -> List[str]:
+        return np.random.choice(list(location_forecasts.keys()), size=self.clients_per_round, replace=False)
+
+
+class CarbonAwareStrategy(Strategy):
+    def __init__(self, clients_per_round: int, max_forecast_duration: int):
+        self.clients_per_round = clients_per_round
+        self.max_forecast_duration = max_forecast_duration
+
+    def select(self, location_forecasts: Dict[str, pd.DataFrame], past_participation: Dict[str, int]) -> List[str]:
+        """Optimize for clients with the least absolute potential to improve their carbon intensity soon.
+
+        For each round, we compute an individual forecast window for each client based on its past participation.
+        Underparticipating clients get a short window, because we want them to participate sooner.
+        Each client gets a score based on its absolute saving potential within the forecast window.
+        The  n clients with the least potential get selected.
+        """
+        participation = np.array(list(past_participation.values()))
+        windows = self._calc_forecast_windows(participation)
+        forecasts = [df["rating"].values for df in location_forecasts.values()]
+        deltas = [self._lowest_delta(fc, w) for fc, w in zip(forecasts, windows)]
+        locations_sorted_by_score = pd.Series(location_forecasts.keys(), index=deltas).sort_index(ascending=False)
+        return locations_sorted_by_score.iloc[:self.clients_per_round].values
+
+    def _calc_forecast_windows(self, participation: np.array):
+        if participation.max() == 0:
+            return np.full(shape=len(participation), fill_value=self.max_forecast_duration)
+        # Clients below 50% of max participation have to participate immediately
+        # Clients above 50% get windows that linearly scale with the max_forecast_duration
+        # Hence, the client with max participation always gets max_forecast_duration
+        normalized = np.maximum(0, 2 * participation / participation.max() - 1)
+        return np.round(normalized * self.max_forecast_duration).astype(int)
+
+    def _lowest_delta(self, forecast: np.array, window: int):
+        """Returns the lowest delta of and forecasted value compared to 'now'"""
+        now = forecast[0]
+        if window == 0:  # TODO document
+            return 100000 - now
+        future = forecast[1:window + 1]
+        min_delta = (future - now).min()
+        return min_delta
