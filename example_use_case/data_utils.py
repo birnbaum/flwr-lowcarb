09e9e46463c40ababb262d4612b806e3259dd776
Dennis Grinwald <dgrinwald93@gmail.com>
2022-10-20 18:50:35 +0000

    update fl train structure

---
+++ example_use_case/data_utils.py
@@ -0,0 +1,116 @@
+import os
+import sys
+import math
+from glob import glob
+from PIL import Image
+from collections import OrderedDict
+from typing import Dict, List, Optional, Tuple
+
+import flwr as fl
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import torch
+import torch.nn as nnqunt
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+
+from torchvision.utils import make_grid
+from torchvision.datasets import CIFAR10
+from torchvision import transforms as T, models
+
+
+def inv_data_transform(img):
+    img = img.permute(1,2,0)
+    img = img * torch.Tensor([0.229, 0.224, 0.225]) + torch.Tensor([0.485, 0.456, 0.406])
+    return img
+
+
+
+class NIH_Dataset(Dataset):
+  def __init__(
+    self, 
+    data_df,
+    transform=None
+    ):
+    self.data_df = data_df
+    self.transform = transform 
+
+  def __len__(
+    self
+    ):
+    return len(self.data_df)
+
+  def __getitem__(
+    self, 
+    idx
+    ):
+    img_file = self.data_df['path'].iloc[idx]
+    img = Image.open(img_file).convert('RGB')
+    label = np.array(self.data_df.iloc[:,-1].iloc[idx], dtype=float)
+    if self.transform:
+        img = self.transform(img)
+
+    return img, label
+
+def get_data_loaders(
+    data_df: pd.DataFrame,
+    global_train_frac: float = 0.8,
+    local_train_frac: float = 0.8,
+    batch_size: int = 32,
+    num_clients: int = 10
+    ):
+  train_data_transform = T.Compose([
+    T.RandomRotation((-20,+20)),
+    # T.Resize((512,512)),
+    T.Resize((224,224)),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])
+                ])
+
+  test_data_transform = T.Compose([
+    T.Resize((512,512)),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])
+                ])
+  
+  train_df, test_df = train_test_split(
+      data_df,
+      test_size = 1 - global_train_frac,
+      random_state = 42,
+      stratify = data_df['Finding Labels'].map( lambda x: x[:4])
+  )
+
+  trainset = NIH_Dataset(
+      train_df,
+      transform = train_data_transform
+      )
+  
+  testset = NIH_Dataset(
+      test_df,
+      transform = test_data_transform
+      )
+
+  # Split training set into `num_clients` partitions to simulate different local datasets
+  partition_size = len(trainset) // num_clients
+  lengths = [partition_size] * num_clients
+  lengths[-1] += len(trainset) - np.sum(lengths)
+  datasets = random_split(trainset, lengths, torch.Generator().manual_seed(42))
+
+  # Split each partition into train/val and create DataLoader
+  trainloaders = []
+  valloaders = []
+  for ds in datasets:      
+      len_train = math.ceil(len(ds) * local_train_frac)
+      len_val = len(ds) - math.ceil(len(ds) * local_train_frac)
+      lengths = [len_train, len_val]
+      ds_train, ds_val = random_split(ds, lengths, torch.Generator().manual_seed(42))
+      trainloaders.append(DataLoader(ds_train, batch_size=batch_size, shuffle=True, drop_last = True))
+      valloaders.append(DataLoader(ds_val, batch_size=batch_size, drop_last = True))
+  testloader = DataLoader(testset, batch_size=batch_size)
+
+  return trainloaders, valloaders, testloader
