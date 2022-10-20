404fc9c566fdd3bb067c247c703981b15b89767d
Philipp Wiesner <wiesner@tu-berlin.de>
2022-10-20 15:58:08 +0200

    we now separately crawl actuals and historical forecasts

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -6,30 +6,36 @@ from crawler import backtest_daterange, LOCATIONS
 # TODO: For now we assume one client per location
 
 
-def run_backtest(history_path: str, strategy: Strategy):
-    with open(history_path, "r") as f:
-        history = pd.read_csv(f, index_col=[0, 1, 2], parse_dates=True)
+def run_backtest(actuals_path: str, forecasts_path: str, strategy: Strategy):
+    with open(actuals_path, "r") as f:
+        actuals = pd.read_csv(f, index_col=[0, 1], parse_dates=True)["rating"]
+    with open(forecasts_path, "r") as f:
+        forecasts = pd.read_csv(f, index_col=[0, 1, 2], parse_dates=True)["rating"]
 
     participation = {location: 0 for location in LOCATIONS}  # counts number of times a location was selected
     emissions = {location: 0 for location in LOCATIONS}  # counts emissions per location
     rounds = 0
     for dt in backtest_daterange():
-        df = history[history.index.get_level_values(1) == dt]  # level 1 is query time
+        df = forecasts[forecasts.index.get_level_values(1) == dt]  # level 1 is query time
         df.index = df.index.droplevel(1)
-        location_forecasts = {location: 0 for location in LOCATIONS}  # important for sort order
+        if df.empty:
+            break
+
+        location_forecasts = {location: None for location in LOCATIONS}  # important for sort order
         for location, location_forecast in df.groupby(level=0):
             location_forecast.index = location_forecast.index.droplevel(0)  # dropping location from index
-            location_forecasts[location] = location_forecast
+            location_forecast[dt] = actuals.loc[(location, dt)]
+            location_forecasts[location] = location_forecast.sort_index()
 
         selected_locations = strategy.select(location_forecasts, participation)
         for location in selected_locations:
             participation[location] += 1
-            emissions[location] += float(location_forecasts[location].loc[dt])
+            emissions[location] += actuals.loc[(location, dt)]
         rounds += 1
 
     return participation, emissions, rounds
 
 
 if __name__ == "__main__":
-    # run_backtest("backtest/history.csv", strategy=RandomStrategy(3))
-    run_backtest("backtest/history.csv", strategy=CarbonAwareStrategy(clients_per_round=3, max_forecast_duration=36))
+    # run_backtest("backtest/actuals.csv", "backtest/forecasts.csv", strategy=RandomStrategy(3))
+    run_backtest("backtest/actuals.csv", "backtest/forecasts.csv", strategy=CarbonAwareStrategy(clients_per_round=3, max_forecast_duration=36))
--- backtest/crawler.py
+++ backtest/crawler.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from dateutil import parser
 
 import pandas as pd
 
@@ -7,81 +6,81 @@ from carbon_sdk_client.openapi_client.api.carbon_aware_api import CarbonAwareApi
 from carbon_sdk_client.openapi_client.api_client import ApiClient
 from carbon_sdk_client.openapi_client.configuration import Configuration
 from carbon_sdk_client.openapi_client.models import EmissionsForecastDTO, EmissionsForecastBatchParametersDTO
-from carbon_sdk_client.openapi_client import ApiException
 
 
 LOCATIONS = ['westcentralus', 'ukwest', 'uksouth', 'westeurope', 'westus', 'australiacentral', 'australiaeast', 'swedencentral', 'norwaywest', 'norwayeast', 'northeurope', 'centralus', 'francesouth', 'francecentral']
+
 FORECAST_FREQ = 15  # in min
-FORECAST_WINDOW = 180  # in min
-BACKTEST_START = "2022-10-13T00:00:00.00"
-BACKTEST_END = "2022-10-15T00:00:00.00"
+FORECAST_WINDOW = 6 * 60  # in min
+BACKTEST_START = "2022-10-12T00:00:00.00"
+BACKTEST_END = "2022-10-17T00:55:00.00"
 
 
 def backtest_daterange():
     return pd.date_range(start=BACKTEST_START, end=BACKTEST_END, freq=f"{FORECAST_FREQ}min")
 
 
-def historic_forecast_ideal(api_instance, location, start_time, end_time) -> pd.DataFrame:
+def historic_actual(api_instance, location, start_time, end_time) -> pd.DataFrame:
     api_response = api_instance.get_emissions_data_for_location_by_time(location, time=start_time, to_time=end_time)
     result = pd.DataFrame([r.to_dict() for r in api_response]).drop(columns={"duration"}).sort_values("time")
     result["time"] = result["time"].dt.tz_localize(None)  # remove timezone
-    result = result[result["time"] >= start_time]  # api sometimes returns older values than start_time
     result["location"] = location  # api returns watttime label instead of input label
-    result["query_time"] = start_time
-    result = result.set_index(["location", "query_time", "time"])
+    result = result.set_index(["location", "time"])
     return result
 
-def historic_forecast(api_instance, location, start_time, end_time) -> pd.DataFrame:
-    start_time = parser.parse(start_time) if (type(start_time) == str) else start_time
-    end_time = parser.parse(end_time) if (type(end_time) == str) else end_time
-    query_time = start_time - timedelta(minutes=5)
 
-
-    if type(location) == str:
-        locations = [location]
-    else:
-        locations = location
-
+def historic_forecast(api_instance, locations, query_time) -> pd.DataFrame:
     emissions_forecast_batch_parameters_dtos = [
         EmissionsForecastBatchParametersDTO(
             requested_at=query_time,
             location=location,
-            data_start_at=start_time,
-            data_end_at=end_time,
+            data_start_at=query_time + timedelta(minutes=FORECAST_FREQ),
+            data_end_at=query_time + timedelta(minutes=FORECAST_WINDOW + FORECAST_FREQ),
             window_size=5,
         )
     for location in locations]
 
-    try:
-        api_response = api_instance.batch_forecast_data_async(emissions_forecast_batch_parameters_dto=emissions_forecast_batch_parameters_dtos, async_req=False)
+    api_response = api_instance.batch_forecast_data_async(
+        emissions_forecast_batch_parameters_dto=emissions_forecast_batch_parameters_dtos, async_req=False)
 
-        results = []
-        for i_entry, entry in enumerate(api_response, 0):
-            result = pd.DataFrame(entry.to_dict()['forecast_data']).drop(columns={"duration"}).sort_values("timestamp")
-            result = result.rename(columns={'timestamp':'time', 'value':'rating'})
-            result["time"] = result["time"].dt.tz_localize(None)  # remove timezone
-            result = result[result["time"] >= start_time]  # api sometimes returns older values than start_time
-            result["location"] = locations[i_entry]  # api returns watttime label instead of input label
-            result["query_time"] = start_time
-            result = result.set_index(["location", "query_time", "time"])
+    results = []
+    for i_entry, entry in enumerate(api_response, 0):
+        result = pd.DataFrame(entry.to_dict()['forecast_data']).drop(columns={"duration"}).sort_values("timestamp")
+        result = result.rename(columns={'timestamp': 'time', 'value': 'rating'})
+        result["time"] = result["time"].dt.tz_localize(None)  # remove timezone
+        result = result[result["time"] >= query_time]  # api sometimes returns older values than start_time
+        result["location"] = locations[i_entry]  # api returns watttime label instead of input label
+        result["query_time"] = query_time
+        result = result.set_index(["location", "query_time", "time"])
+        results.append(result)
 
-            results.append(result)
+    return pd.concat(results)
 
-
-        return pd.concat(results)
-
-    except ApiException as e:
-        print("Exception when calling CarbonAwareApi->batch_forecast_data_async: %s\n" % e)
 
 def main():
     with ApiClient(configuration=Configuration(host="https://carbon-aware-api.azurewebsites.net")) as api_client:
         api_instance = CarbonAwareApi(api_client)
+
+        # crawl historical actuals
         dfs = []
         for location in LOCATIONS:
-            for dt in backtest_daterange():
+            for dt in pd.date_range(start=BACKTEST_START, end=BACKTEST_END, freq=f"1d"):
                 print(f"Query {dt} for {location}")
-                dfs.append(historic_forecast(api_instance, location, dt, dt + timedelta(minutes=FORECAST_WINDOW)))
-        pd.concat(dfs).to_csv("backtest/history.csv")
+                dfs.append(historic_actual(api_instance, location, dt, dt + timedelta(days=1)))
+        df = pd.concat(dfs)
+        df = df.groupby(level=df.index.names).first()  # remove duplicates
+        df = df[df.index.get_level_values(1).minute.isin([0, 15, 30, 45])]  # downsample to 15min
+        df.to_csv("backtest/actuals.csv")
+
+        # crawl historical forecasts
+        dfs = []
+        for dt in backtest_daterange():
+            print(f"Query historical forecast at {dt}")
+            dfs.append(historic_forecast(api_instance, LOCATIONS, dt))
+        df = pd.concat(dfs)
+        # TODO
+        df = df[df.index.get_level_values(2).minute.isin([0, 15, 30, 45])]  # downsample to 15min
+        df.to_csv("backtest/forecasts.csv")
 
 
 if __name__ == "__main__":
