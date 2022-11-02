d11b15615ab4dbdb5ead1c8381465b95b0c32fd7
Philipp Wiesner <wiesner@tu-berlin.de>
2022-11-02 20:10:22 +0100

    package now correctly installs the OpenAPI client

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
+
+open_api_packages = [f"lowcarb.python_carbon_sdk_client.{package}"
+                     for package in find_packages(where="lowcarb/python_carbon_sdk_client", exclude=["test", "tests"])]
+
 
 if __name__ == "__main__":
     setup(
@@ -16,7 +20,7 @@ if __name__ == "__main__":
         long_description_content_type='text/markdown',
         keywords=["carbon awareness", "federated learning", "client selection", "flower"],
         url="https://github.com/birnbaum/lowcarb",
-        packages=["lowcarb"],
+        packages=["lowcarb"] + open_api_packages,
         license="MIT",
         python_requires=">=3.7",
         setup_requires=['setuptools_scm'],
